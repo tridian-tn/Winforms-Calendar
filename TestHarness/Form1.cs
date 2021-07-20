6d1868081c02132e092549a0ef4f9997f35a7f83
Trystan Negus <trystan@tridian.net>
2021-07-20 15:43:09 +0100

    Migrate to .NET 5

--- TestHarness/Form1.cs
+++ TestHarness/Form1.cs
@@ -1,10 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
 using System.Windows.Forms;
 
 using WindowsFormsCalendar;
@@ -15,7 +10,7 @@ namespace TestHarness
     {
         #region Fields
 
-        private List<CalendarItem> _items = new List<CalendarItem>();
+        private List<CalendarItem> _items = new();
 
         #endregion
 
